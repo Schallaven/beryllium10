170f6b3a8c96dfa530c654f358c5bcd5c50eb289
schallaven <schallaven@74b80ae6-4964-4e25-a8cb-022e9688670a>
2014-01-26 16:25:27 +0000

    Update der Versionsdatei

--- Beryllium10/Code/AutoBuild.h
+++ Beryllium10/Code/AutoBuild.h
@@ -2,8 +2,8 @@
 #define AUTOBUILD_H
 // change the FALSE to TRUE for autoincrement of build number
 #define INCREMENT_VERSION TRUE
-#define FILEVER        2,2,0,1005
-#define PRODUCTVER     2,2,0,1005
-#define STRFILEVER     "2,2,0,1005\0"
-#define STRPRODUCTVER  "2,2,0,1005\0"
+#define FILEVER        2,2,0,1013
+#define PRODUCTVER     2,2,0,1013
+#define STRFILEVER     "2,2,0,1013\0"
+#define STRPRODUCTVER  "2,2,0,1013\0"
 #endif // AUTOBUILD_H
