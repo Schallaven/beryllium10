087696984b26c3fc8c9b8921ecd6fcb3c0158852
Sven Kochmann <sven.kochmann@gmx.de>
2022-06-06 15:49:27 +0200

    Updated license information in each file

--- code/berylliummainframe_identifiers.h
+++ code/berylliummainframe_identifiers.h
@@ -1,34 +1,21 @@
-﻿// **********************************************************************************
-//   This file is part of Beryllium¹º.
-//
-//   Beryllium¹º is free software: you can redistribute it and/or modify
-//   it under the terms of the GNU General Public License as published by
-//   the Free Software Foundation, either version 3 of the License, or
-//   (at your option) any later version.
-//
-//   Beryllium¹º is distributed in the hope that it will be useful,
-//   but WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//   GNU General Public License for more details.
-//
-//   You should have received a copy of the GNU General Public License
-//   along with Beryllium¹º.  If not, see <http://www.gnu.org/licenses/>.
-//
-//   Diese Datei ist Teil von Beryllium¹º.
-//
-//   Beryllium¹º ist Freie Software: Sie können es unter den Bedingungen
-//   der GNU General Public License, wie von der Free Software Foundation,
-//   Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
-//   veröffentlichten Version, weiterverbreiten und/oder modifizieren.
-//
-//   Beryllium¹º wird in der Hoffnung, dass es nützlich sein wird, aber
-//   OHNE JEDE GEWÄHELEISTUNG, bereitgestellt; sogar ohne die implizite
-//   Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
-//   Siehe die GNU General Public License für weitere Details.
-//
-//   Sie sollten eine Kopie der GNU General Public License zusammen mit
-//   Beryllium¹º erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
-// **********************************************************************************
+﻿/***********************************************************************************
+   Copyright (C) 2022 by Sven Kochmann
+
+   This file is part of Beryllium¹º.
+
+   Beryllium¹º is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Beryllium¹º is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Beryllium¹º.  If not, see <http://www.gnu.org/licenses/>.
+***********************************************************************************/
 
 // Defines für die Menus/Toolbar
 // -----------------------------
@@ -45,7 +32,7 @@
 // Ansicht
 #define beID_VIEW_TOOLBAR		6001
 #define beID_VIEW_STATUSBAR		6002
-	
+
 #define beID_VIEW_FIRST			6101	// Erste Ansicht
 #define beID_VIEW_SAFETYSHEET	6101	// Ansicht: Gruppenbetriebsanweisung
 #define beID_VIEW_DATASHEET		6102	// Ansicht: Datenblatt für jede Substanz
--- code/berylliumviewmanager.cpp
+++ code/berylliumviewmanager.cpp
@@ -1,35 +1,21 @@
-﻿// **********************************************************************************
-//   This file is part of Beryllium¹º.
-//
-//   Beryllium¹º is free software: you can redistribute it and/or modify
-//   it under the terms of the GNU General Public License as published by
-//   the Free Software Foundation, either version 3 of the License, or
-//   (at your option) any later version.
-//
-//   Beryllium¹º is distributed in the hope that it will be useful,
-//   but WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//   GNU General Public License for more details.
-//
-//   You should have received a copy of the GNU General Public License
-//   along with Beryllium¹º.  If not, see <http://www.gnu.org/licenses/>.
-//
-//   Diese Datei ist Teil von Beryllium¹º.
-//
-//   Beryllium¹º ist Freie Software: Sie können es unter den Bedingungen
-//   der GNU General Public License, wie von der Free Software Foundation,
-//   Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
-//   veröffentlichten Version, weiterverbreiten und/oder modifizieren.
-//
-//   Beryllium¹º wird in der Hoffnung, dass es nützlich sein wird, aber
-//   OHNE JEDE GEWÄHELEISTUNG, bereitgestellt; sogar ohne die implizite
-//   Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
-//   Siehe die GNU General Public License für weitere Details.
-//
-//   Sie sollten eine Kopie der GNU General Public License zusammen mit
-//   Beryllium¹º erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
-// **********************************************************************************
+﻿/***********************************************************************************
+   Copyright (C) 2022 by Sven Kochmann
 
+   This file is part of Beryllium¹º.
+
+   Beryllium¹º is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Beryllium¹º is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Beryllium¹º.  If not, see <http://www.gnu.org/licenses/>.
+***********************************************************************************/
 
 #include "beryllium.h"
 
